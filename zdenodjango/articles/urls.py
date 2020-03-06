22f4217063248e7f92b3899fc11dbcc43e693343
ZDENKO MUCINA <MucinaZ@cardiff.ac.uk>
2020-03-06 18:06:05 +0000

    keep calm

--- zdenodjango/articles/urls.py
+++ zdenodjango/articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list),
+    url(r'^$', views.article_list, name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
 ]
